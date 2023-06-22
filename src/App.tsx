50598518208cbf3a9ca35104f4d4c2b7a7e39930
robertcunningham <robertcunningham@ford.com>
2023-06-22 11:21:45 -0700

    reminify tiny react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-	    
+      {"Hello World!"}
     </div>
   );
 }
-
-export default App;
